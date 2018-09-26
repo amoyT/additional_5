0a92486d0a36fb45e30a84ea99edb5132b552568
Tyoma Zhytsko <zhytsko.a@gmail.com>
2018-09-26 22:51:27 +0300

    Function Check was implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let stack = [];
+    let openBr = [];
+    let closeBr = [];
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        openBr[i] = bracketsConfig[i][0];
+        closeBr[i] = bracketsConfig[i][1];
+    }
+    stack.push(str[0]);
+    for (var i = 1; i < str.length; i++) {
+        if (openBr.includes(str[i]) && !closeBr.includes(str[i]))
+            stack.push(str[i]);
+        else {
+            // if (stack.length === 0)
+            //     return false;
+            //let last = stack.pop();
+            if (stack[stack.length-1] === openBr[closeBr.indexOf(str[i])])
+                stack.pop();
+            else {
+                stack.push(str[i]);
+            }
+            // if (str[i] === closeBr[openBr.indexOf(last)])
+            //     continue;
+            // else return false;
+        }
+    }
+    return stack.length === 0
 }
